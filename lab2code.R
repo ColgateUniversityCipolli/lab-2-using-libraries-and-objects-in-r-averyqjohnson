c59b7dd3ba612ea5eca8e1b3ae8d601d03044df8
averyqjohnson <aqjohnson@colgate.edu>
2025-01-28 14:28:42 -0500

    Task 1 through Step 2

--- lab2code.R
+++ lab2code.R
@@ -11,7 +11,13 @@
 
 # Step 1: Find the command required to list all of the directories in a
 # specific directory and use it to find all subdirectories of MUSIC/
-library("stringr")
 
-music.directories <- list.dirs("Music/", recurisve = TRUE)
+library("stringr")
+# help("stringr")
+
+music.directories <- list.dirs("Music")
+
+# Step 2: Use the str_count() function to count the numbner of (/) in each
+# directory
+num_slashes <- str_count(music.directories, "/")
 
